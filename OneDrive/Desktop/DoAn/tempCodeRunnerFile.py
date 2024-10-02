f4b63249ab8d08ab7facdce334e73336b99c6410
Ninh216 <127207129+Ninh216@users.noreply.github.com>
2024-10-02 21:59:08 +0700

    Delete OneDrive/Desktop/DoAn/tempCodeRunnerFile.py

--- OneDrive/Desktop/DoAn/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-
-        if num > maxOccur:
-            maxOccur = num
-            ans = label
-    return ans
-
-if __name__ == "__main__":
-    trainSet, testSet = loadData("./dulieu.csv")
-    numOfRightAnwser = 0
